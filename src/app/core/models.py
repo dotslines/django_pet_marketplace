00b80d08d2568f04cf827d8577d246c502336927
a s <example@example.com>
2024-01-05 15:26:27 +0400

    GeneralData singletoon model created for keeping site settings or general data

--- src/app/core/models.py
+++ src/app/core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from utils.models import Singleton
+
+
+class GeneralData(Singleton):
+    """
+    General site settings, data.
+    """
+    email = models.EmailField(
+        _("Marketplace's contact email address."),
+        max_length=20,
+        blank=True,
+        null=True
+    )
+    phone = models.CharField(
+        _("Marketplace's contact phone number."),
+        max_length=20,
+        blank=True,
+        null=True
+    )
+    telegram = models.CharField(
+        _("Marketplace's contact telegram link."),
+        max_length=20,
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = _('Marketplace general data, site settings')
+        verbose_name_plural = _('Marketplace general data, site settings')
