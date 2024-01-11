e3e1e739d55637d9a8127f4bf305e0a2e4d81326
a s <example@example.com>
2024-01-11 15:23:13 +0400

    core admin for GeneralData

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+
+from utils.admin import SingletonAdmin
+
+from .models import GeneralData
+
+
+@admin.register(GeneralData)
+class GeneralDataAdmin(SingletonAdmin):
+    change_form_template = 'admin/change_form_no_deletion.html'
